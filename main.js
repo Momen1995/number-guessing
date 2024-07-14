1afd574dc82866b76d9bc64a912059122a9dda44
Momen1995 <momensrk@gmail.com>
2024-07-14 21:16:09 +0600

    array and some calculation add

--- main.js
+++ main.js
@@ -4,35 +4,44 @@ const inputNum = document.getElementById("input-number");
 const submitBtn = document.querySelector(".btn-submit");
 const startBtn = document.querySelector(".btn-start");
 const measure = document.querySelector(".measure");
+const length = document.querySelector(".length");
 const guessingNum = document.getElementById("guessing-num");
 
-let SECRET_NUMBER = Math.trunc(Math.random() * 20 + 1);
+const allGuessNumber = [];
+let SECRET_NUMBER = Math.trunc(Math.random() * 30 + 1);
 console.log(SECRET_NUMBER);
 
+// display feedback text
 function displayText(text) {
   measure.textContent = text;
 }
 
 submitBtn.addEventListener("click", function () {
   const input = Number(inputNum.value);
-  
+
+  // Check if the guess is too high, too low, or correct
   if (input > SECRET_NUMBER) {
     displayText("Too High😃");
-    guessingNum.textContent = input;
   } else if (input < SECRET_NUMBER) {
     displayText("Too Low!😐");
-    guessingNum.textContent = input;
   } else if (input === SECRET_NUMBER) {
-    displayText("You win!🏆");
-    guessingNum.textContent = input;
+    displayText("Congrats! You win🏆");
+    length.textContent = allGuessNumber.length;
 
-    startBtn.removeAttribute("disabled")
+    startBtn.removeAttribute("disabled");
+    submitBtn.setAttribute("disabled", "");
   }
+  allGuessNumber.push(input);
+  guessingNum.textContent = allGuessNumber.join(",");
+  inputNum.value = "";
 });
 
-startBtn.addEventListener("click",function(){
-  SECRET_NUMBER = Math.trunc(Math.random() * 20 + 1);
-  console.log(SECRET_NUMBER)
-  displayText("")
-  guessingNum.textContent = ""
-})
+// Event listener for the start button
+startBtn.addEventListener("click", function () {
+  SECRET_NUMBER = Math.trunc(Math.random() * 30 + 1);
+  console.log(SECRET_NUMBER);
+  displayText("");
+  guessingNum.textContent = "";
+  startBtn.setAttribute("disabled", "");
+  submitBtn.removeAttribute("disabled");
+});
